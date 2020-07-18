2fbba80f0d9643045e097fbd578596a4a31525e1
blueeyezhy <blueeye_zhy@outlook.com>
2020-07-18 13:00:50 +0800

    add start_prod.sh

---
+++ scripts/start_prod.sh
@@ -0,0 +1,5 @@
+docker login -u prs-os -p pressone dockerhub.qingcloud.com
+docker pull dockerhub.qingcloud.com/pressone/atom
+
+docker-compose -f docker-compose.yml down --remove-orphans &&
+docker-compose -f docker-compose.yml up -d
